febce18642fd9b692bfdfbf89c5740de03d0778c
aredjil <bellred499@gmail.com>
2024-12-25 16:10:19 +0100

    working code

---
+++ Ising/vis_utils.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3 
+import numpy as np 
+import matplotlib.pyplot as plt 
+from tqdm import tqdm
+from PIL import Image 
+import os 
+
+def create_gif(image_folder, output_path, m_range, duration=100):
+    images = []
+    try:
+        os.makedirs(output_path, exist_ok=True)
+        print(f"Created {output_path}")
+    except Exception as e: 
+        print(f"An execption occured {e}")
+    try:
+        print("Generating the GIF...")
+        for m in m_range:
+            file_name = f"lattice_{m}.png"  
+            file_path = os.path.join(image_folder, file_name)
+            if os.path.exists(file_path):
+                img = Image.open(file_path)
+                images.append(img)
+            else:
+                print(f"File not found: {file_path}")
+        
+        if images:
+            images[0].save(
+                output_path+"/ising_you_sing.gif",
+                save_all=True,
+                append_images=images[1:],  
+                duration=duration  
+            )
+            print(f"GIF saved at {output_path}")
+        else:
+            print("No images were found to create a GIF.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+def gen_gif(max_m:int, min_m:int, step_m:int, duration:int, hist_path:str):
+    
+    output_gif = "./gifs"  
+    m_range = range(min_m, max_m, step_m) 
+    duration_per_frame = duration  
+
+    create_gif(hist_path, output_gif, m_range, duration=duration_per_frame)
+
+def remove_plots(max_m:int, min_m:int, step_m:int, path):
+    for m in range(min_m, max_m, step_m):
+        file_path = f"./{path}/lattice_{m}.png"
+        if os.path.exists(file_path):
+            try:
+                os.remove(file_path)
+            except Exception as e: 
+                print(f"An error occured while removing {file_path} {e}")
+    os.rmdir(path)
+    print(f"removed {path}")
--- src/run.py
+++ src/run.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 import numpy as np 
 from Ising.Ising import *
-
+from Ising.vis_utils import * 
+#NOTE: The code needs to be generlized. 
+#NOTE: Add magnetisation vs temperature 
 def main():
-    print("Hello sweet world")
-    hello()
+    n, seed, p = 100, 42, 0.8
+    kb, J, T = 1.0, 1.0, 1.0
+    t_max = n * n 
+    ising = Ising(n=n, 
+                  p=p,
+                  kb=kb, 
+                  J=J,
+                  Temp=T,
+                  t_max = t_max,
+                  seed=seed)
+    m = ising.solve()
 if __name__ =="__main__":
+    path = "./plots" # Path for a temporary directory where to save plots 
     main()
+    gen_gif(10000, 0, 10, 10, path)
+    #NOTE: there is a minro error in the removing file, fix it 
+    # remove_plots(1000, 0, 50, path)
