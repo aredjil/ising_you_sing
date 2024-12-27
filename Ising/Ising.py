a1ca17d288e4bcaf1bc4393488b1b04352a444cb
aredjil <bellred499@gmail.com>
2024-12-27 13:14:05 +0100

    parallelized the code with numba now it is 26x faster, and added list of dependencies

--- Ising/Ising.py
+++ Ising/Ising.py
@@ -4,10 +4,13 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 from tqdm import tqdm 
 import os 
+from numba import njit 
+from Ising.vis_utils import * 
+
 cmap = mcolors.ListedColormap(["#99d8c9", "#2ca25f"])
+
 #NOTE: COMMENT THE CLASS and its methods. 
 class Ising:
-
     def __init__(self, n:int, p:float|None, kb:float, J:float, Temp:float, t_max:float, seed:np.int32|None):
         
         self._n = n # Dimension of the lattice of the lattice 
@@ -24,10 +27,10 @@ class Ising:
         n = self._n
         p = self._p  
         return np.random.choice([-1, 1], size=(n, n), p=[p, 1-p])
-    
-    def calculate_energy(self, lattice):
-        J = self._J 
-        n = self._n 
+    @staticmethod
+    @njit 
+    def calculate_energy(lattice, J):
+        n = lattice.shape[0] 
         energy = 0
         for i in range(n):
             for j in range(n):
@@ -37,34 +40,32 @@ class Ising:
                             lattice[(i-1)%n, j] + lattice[i, (j-1)%n]
                 energy += -J * S * neighbors
         return energy / 2  
-    def metropolis_step(self, lattice):
-        n = self._n         
-        beta = self._beta 
-        J = self._J 
-        for _ in range(n**2):
+    @staticmethod
+    @njit
+    def metropolis_step(lattice, n, beta, J):
+        for _ in range(n*n):
             i, j = np.random.randint(0, n, 2)  
             S = lattice[i, j]
-
             neighbors = lattice[(i+1)%n, j] + lattice[i, (j+1)%n] + \
                         lattice[(i-1)%n, j] + lattice[i, (j-1)%n]
             dE = 2 * J * S * neighbors
             if dE < 0 or np.random.rand() < np.exp(-beta * dE):
                 lattice[i, j] *= -1
-    def simulate(self):
-
-        lattice = self._lattice.copy()
-        T = self._Temp
-        steps = self._t_max
-        J = self._J 
-        n = self._n
+    @staticmethod
+    # @njit
+    def simulate(lattice, T, beta, J, t_max):
+        lattice = lattice.copy()
+        n = lattice.shape[0]
         beta = 1 / T
         magnetization = []
         energy = []
-        for step in tqdm(range(steps), desc="Computing"):
-            self.metropolis_step(lattice)
-            if step > steps // 2:  # Collect data after equilibration
+        for step in tqdm(range(t_max), desc="Computing"):
+            Ising.metropolis_step(lattice, n, beta, J)
+            if step > t_max // 3:  # Collect data after equilibration
                 magnetization.append(np.abs(np.sum(lattice)) / (n*n))
-                energy.append(self.calculate_energy(lattice) / (n*n))
+                energy.append(Ising.calculate_energy(lattice, J) / (n*n))
         return lattice, np.mean(magnetization), np.mean(energy), np.var(energy) * beta**2, np.var(magnetization) * beta
+    def run_simulation(self):
+        return Ising.simulate(self._lattice, self._Temp, self._beta, self._J, self._t_max)
 
 
--- Ising/vis_utils.py
+++ Ising/vis_utils.py
@@ -3,8 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from tqdm import tqdm
 from PIL import Image 
+import matplotlib.colors as mcolors
 import os 
-#NOTE: Create the output folder if it does not already exists. 
+
+cmap = mcolors.ListedColormap(["#99d8c9", "#2ca25f"])
+
 def create_gif(input_files_path:str, input_file_name:str, output_path:str, output_file_name:str, m_range:int, duration:int=100):
     """
     Generate a gif using saved plots. 
@@ -76,3 +79,50 @@ def remove_plots(max_m:int, min_m:int, step_m:int, path):
                 print(f"An error occured while removing {file_path} {e}")
     os.rmdir(path)
     print(f"removed {path}")
+
+def plot_fig(temp, lattice, mag, energies, heat_capacity, iter):
+    #NOTE: Create the output folder if it does not already exists. 
+    n = lattice.shape[0]
+    fig, ax = plt.subplots(ncols=2, nrows=2, figsize=(12, 12))
+    cax = ax[0, 0].imshow(lattice, cmap=cmap)
+    ax[0, 0].set_xticks([])
+    ax[0, 0].set_yticks([])
+    ax[0, 0].grid(which='minor', color='gray', linestyle='-', linewidth=0.5)
+    ax[0, 0].tick_params(which='minor', size=0)
+    ax[0, 0].set_title(f"Lattice Configuration")
+    cbar = plt.colorbar(cax, ax=ax[0, 0], ticks=[-1, 1])
+    cbar.ax.set_yticklabels(['-1 (Down)', '1 (Up)'])
+
+    ax[0, 1].scatter(temp[iter], mag[iter], color="red")
+    ax[0, 1].plot(temp[:iter + 1], mag[:iter + 1],  label="$<M>$", color='blue', alpha=0.5)
+    ax[0, 1].set_xlim(temp[0], temp[-1])
+    ax[0, 1].set_ylim(0, 1.0)
+    ax[0, 1].set_title("<M> vs T")
+    ax[0, 1].set_xlabel("T")
+    ax[0, 1].set_ylabel("<M>")
+    ax[0, 1].grid()
+    ax[0, 1].legend()
+
+    ax[1, 0].scatter(temp[iter], energies[iter], color="red")
+    ax[1, 0].plot(temp[:iter + 1], energies[:iter + 1], label="E", color='blue', alpha=0.5)
+    ax[1, 0].set_xlim(temp[0], temp[-1])
+    ax[1, 0].set_ylim(energies[0], 0.0)
+    ax[1, 0].set_title("E vs T")
+    ax[1, 0].set_xlabel("T")
+    ax[1, 0].set_ylabel("E")
+    ax[1, 0].grid()
+    ax[1, 0].legend()
+
+    ax[1, 1].scatter(temp[iter], heat_capacity[iter], color="red")
+    ax[1, 1].plot(temp[:iter + 1], heat_capacity[:iter + 1], label="$C_v$", color='blue', alpha=0.5)
+    ax[1, 1].set_xlim(temp[0], temp[-1])
+    # ax[1, 1].set_ylim(0, 1.0)
+    ax[1, 1].set_title("$C_v$ vs T")
+    ax[1, 1].set_xlabel("T")
+    ax[1, 1].set_ylabel("$C_v$")
+    ax[1, 1].grid()
+    ax[1, 1].legend()
+
+    fig.suptitle(f"{n}*{n} Lattice \nT={temp[iter]:.2f}")
+    plt.tight_layout()
+    return fig
